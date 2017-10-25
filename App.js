438e9669c1f6a4a2248e3ca3ff385b79de4b9fcc
Alessandro <alessandro.grunwald@gmail.com>
2017-10-24 17:50:40 -0700

    major bug

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Text, View, Button, ScrollView } from 'react-native';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
 
 //Importing Screens
@@ -8,7 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import DrawerMenu from './screens/MenuScreen';
 
-export default Proxy = DrawerNavigator({
+export default DrawerNavigator({
     Login: { 
         screen: LoginScreen,
         navigationOptions: ({navigation}) => ({

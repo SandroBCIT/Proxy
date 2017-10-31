9bd0b913df0483a357735318bc9af014c04c67d7
Alessandro <alessandro.grunwald@gmail.com>
2017-10-31 00:01:59 -0700

    radius and distanceChecker working. sets off pop up window for now

--- screens/comp/Map.js
+++ screens/comp/Map.js
@@ -12,7 +12,9 @@ const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 var targetMarker = null;
 var locRefresh = null;
 var setRadiusBtn = null;
+var remRadiusBtn = null;
 var radiusMarker = null;
+var coverView = null;
 
 class Map extends Component {
     constructor(props) {
@@ -29,16 +31,18 @@ class Map extends Component {
                 latitude: 0,
                 longitude: 0
             },
-            circleRadius: 300,
+            circleRadius: 10,
             circleIndex: 9999,
             testPosition: {
                 latitude: 49.2827,
                 longitude: -123.1207
-            }          
+            },
+            checkDistance: false,
         }
         
         this.myCallback = this.myCallback.bind(this);
         this.setRadius = this.setRadius.bind(this);
+        this.remRadius = this.remRadius.bind(this);
         this.onLongPress = this.onLongPress.bind(this);
     }
     
@@ -52,8 +56,8 @@ class Map extends Component {
         locRefresh = setInterval(()=>{
             
             navigator.geolocation.getCurrentPosition((position) => {
-                var lat = /*49.2827*/ parseFloat(position.coords.latitude)
-                var long = /*-123.1207*/ parseFloat(position.coords.longitude)
+                var lat = parseFloat(position.coords.latitude)
+                var long = parseFloat(position.coords.longitude)
                 var latDelta2 = latDelta
                 var longDelta2 = longDelta
                 
@@ -78,11 +82,38 @@ class Map extends Component {
                 longDelta = parseFloat(position.coords.longitudeDelta)
 
             }, (error)=> this.setState({alertMsg:alert}), {enableHighAccuracy: true, timeout: 1000, maximumAge: 500})
+            
+            if(this.state.checkDistance == true){
+                //converts radius from meters to lat/long scale
+                var radius = (0.00001*(this.state.circleRadius));
+                
+                var xLoc = this.state.locationMarkerPosition['longitude'];
+                var yLoc = this.state.locationMarkerPosition['latitude'];
+                var xDest = this.state.targetMarkerPosition['longitude'];
+                var yDest = this.state.targetMarkerPosition['latitude'];
+
+                //calculates distance from location to targetMarker
+                var distance = Math.sqrt(Math.pow((xLoc-xDest),2)+Math.pow((yLoc-yDest),2));
+                
+                if(distance <= radius){
+                    alert('You have ARRIVED');
+                    this.setState({
+                        checkDistance: false        
+                    });
+        
+                    radiusMarker = null;
+                    remRadiusBtn = null;
+                    coverView = null;
+                }
+            }
+            
         }, 500); 
     }
     
     //grabs location data (long/lat) from MapSearch component and updates screenPosition
     myCallback(data){
+        radiusMarker = null;
+        
         var latData = parseFloat(data['latitude']);
         var longData = parseFloat(data['longitude']);
         var latDeltaData = LATITUDE_DELTA;
@@ -104,6 +135,7 @@ class Map extends Component {
         targetMarker =  <MapView.Marker 
                             coordinate={this.state.targetMarkerPosition}
                         />;
+        
         setRadiusBtn =  <Button
                             style = {styles.setRadiusBtn}
                             title='Set Radius'
@@ -113,6 +145,8 @@ class Map extends Component {
     }
     
     onLongPress(data){
+        radiusMarker = null;  
+        
         var longPressLat = parseFloat(JSON.stringify(data.nativeEvent.coordinate['latitude']));
         var longPressLong = parseFloat(JSON.stringify(data.nativeEvent.coordinate['longitude']));
         
@@ -127,21 +161,50 @@ class Map extends Component {
         
         targetMarker =  <MapView.Marker 
                             coordinate={{latitude: longPressLat, longitude: longPressLong}}
-                        />;   
+                        />; 
+
+        setRadiusBtn =  <Button
+                            style = {styles.setRadiusBtn}
+                            title='Set Radius'
+                            color='green'
+                            onPress={this.setRadius}
+                        />;  
     }
     
     setRadius(){
+        this.setState({
+            checkDistance: true   
+        });
+        
         radiusMarker =  <MapView.Circle 
                             center={{latitude: this.state.targetMarkerPosition.latitude, longitude: this.state.targetMarkerPosition.longitude}}
                             radius={this.state.circleRadius}
                             zIndex={this.state.circleIndex}
                             fillColor='lightgreen'
                         />;
-    }
                             
+        remRadiusBtn =  <Button
+                            style = {styles.setRadiusBtn}
+                            title='Cancel'
+                            color='red'
+                            onPress={this.remRadius}
+                        />; 
+
+        coverView =     <View
+                            style = {styles.coverView}
+                        />;
+    }
     
+    remRadius(){
+        this.setState({
+            checkDistance: false   
+        });
+        
+        radiusMarker = null;
+        remRadiusBtn = null;
+        coverView = null;
+    }
     
-    //before component gets trashed
     componentWillUnmount(){
         clearInterval(locRefresh);
     }
@@ -161,7 +224,6 @@ class Map extends Component {
                     loadingEnabled={true}
                     onLongPress={(data) => this.onLongPress(data)}
                 >
-                
             
                     {radiusMarker}
                     {targetMarker}
@@ -176,7 +238,9 @@ class Map extends Component {
                     </MapView.Marker>
                 </MapView>
                 <MapSearch hamburgerFunction={this.props.hamburgerFunction} callbackFromParent={this.myCallback} />
+                {coverView}
                 {setRadiusBtn}
+                {remRadiusBtn}
             </View>
         );
     }
@@ -216,9 +280,13 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         backgroundColor: '#007AFF'
     },
-    setRadiusBtn: {
-              
+    coverView: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'rgba(0,0,0,0.2)'
     }
+    
 });
 
 export default Map;
